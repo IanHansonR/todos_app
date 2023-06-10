cb6d6a0d79a372d2fce35dcadced1639252e5997
Ian <ian.hanson.r@gmail.com>
2023-06-10 11:54:24 +1000

    add functions of retrieving and writing todos

--- functions.py
+++ functions.py
@@ -1,2 +1,19 @@
-print('hi')
+FILEPATH = "todos.txt"
 
+
+def get_todos(filepath=FILEPATH):
+    """Read text file and return list of items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write items to the text file for storage."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print('hello')
+    print(get_todos())
