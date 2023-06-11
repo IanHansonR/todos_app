a5ce6e5ca93ffedc7aa2b31ed3038c9e6f29b33d
Ian <ian.hanson.r@gmail.com>
2023-06-11 16:11:55 +1000

    Implement the Complete button.

--- todos.py
+++ todos.py
@@ -7,12 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 
 
 window = sg.Window("My Todos",
                    layout=[[label],
                            [input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button],
+                           [complete_button]],
                    font=('Helvetica', 15))
 while True:
     event, values = window.read()
@@ -34,6 +36,17 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos']. update(values=todos)
+        case "Complete":
+            todo_completed = values['todos'][0]
+            todos = functions.get_todos()
+            index = todos.index(todo_completed)
+            del todos[index]
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            print(todo_completed)
+            print(index)
+            print('hello')
+
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
